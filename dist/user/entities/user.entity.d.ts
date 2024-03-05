0b3443d26ba0f1706560e00742ba7bfdf5c1dc79
Abu Huraira <iamhuraira429@gmail.com>
2024-03-05 17:18:57 +0500

    chore: cyclic 5

--- dist/user/entities/user.entity.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class User {
-    name: string;
-    email: string;
-    password: string;
-    role: string;
-}
--- dist/user/user.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { UsersService } from './user.service';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UsersService);
-}
